217bb15fa97de2fb7262707baa9350926bdaa044
Nate Marti <nate_marti@sil.org>
2022-04-30 17:12:43 +0100

    adjust desktop interface colors; clean out old package files

--- install-files/file-manager/actions/open_new_text_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-name="new file"
-if [[ $LANGUAGE == 'fr_FR' ]]; then
-    name="nouveau fichier"
-fi
-gedit "$1/$name"
--- install-files/file-manager/actions/resize_pictures_to_4x6_200dpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while (( $# )); do
-  DIRPATH=$(dirname "$1")
-  FILENAME=$(basename "$1" | sed 's@\.\([^\.]\+\)$@\.resized\.\1@')
-  
-  /usr/bin/convert "$1" -resize 1200x800\> "${DIRPATH}/${FILENAME}"
-
-  shift
-done
