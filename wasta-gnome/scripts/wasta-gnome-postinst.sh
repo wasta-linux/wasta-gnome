a5ee555298dbfa6b67b0c184593ca1eaba483b4f
Nate Marti <nate_marti@sil.org>
2022-06-18 09:25:55 +0100

    clean out old code

--- wasta-gnome/scripts/wasta-gnome-postinst.sh
+++ wasta-gnome/scripts/wasta-gnome-postinst.sh
@@ -27,7 +27,6 @@ fi
 # ------------------------------------------------------------------------------
 # Initial setup
 # ------------------------------------------------------------------------------
-
 echo
 echo "*** Script Entry: wasta-gnome-postinst.sh"
 echo
@@ -65,15 +64,6 @@ EOF
 	# Change GDM3 greeter background color.
 	#	Ref: https://github.com/thiggy01/change-gdm-background/blob/master/change-gdm-background
 	/usr/share/wasta-gnome/scripts/change-gdm3-background.sh '#3C3C3C'
-
-	# Copy wasta-login.sh to GDM3 PostLogin/Default.
-	#gdm_default=/etc/gdm3/PostLogin/Default
-	#if [[ -e $gdm_default ]]; then
-	#	# Have to remove already-linked previous version before copying new version.
-	#	rm $gdm_default
-	#fi
-	#wasta_login=/usr/share/wasta-multidesktop/scripts/wasta-login.sh
-	#cp -l "$wasta_login" /etc/gdm3/PostLogin/Default
 fi
 
 # Add Wasta icon to slick-greeter desktop entry if slick-greeter is installed.
@@ -91,29 +81,7 @@ fi
 # ------------------------------------------------------------------------------
 # Dconf / Gsettings Default Value adjustments
 # ------------------------------------------------------------------------------
-echo
 echo "*** Updating dconf / gsettings default values"
-echo
-
-# Updating dconf before GNOME schemas because they depend on its entries.
-# jammy: Not needed when not creating app groups.
-# dconf update
-
-# GNOME Extension schemas: separate location from System schemas (compile if individual schemas folders exist).
-# jammy: Not needed when adding extensions as individual debs.
-# extensions_schemas=(
-# 	/usr/share/gnome-shell/extensions/applications-overview-tooltip@RaphaelRochet/schemas/
-# 	/usr/share/gnome-shell/extensions/dash-to-panel@jderose9.github.com/schemas/
-# 	/usr/share/gnome-shell/extensions/drive-menu@gnome-shell-extensions.gcampax.github.com/schemas/
-# 	/usr/share/gnome-shell/extensions/ding@rastersoft.com/schemas/
-# 	/usr/share/gnome-shell/extensions/panel-osd@berend.de.schouwer.gmail.com/schemas/
-# 	/usr/share/gnome-shell/extensions/windowIsReady_Remover@nunofarruca@gmail.com/schemas/
-# )
-# for schema in "${extensions_schemas[@]}"; do
-# 	if [[ -d "$schema" ]]; then
-# 		glib-compile-schemas "$schema" >/dev/null 2>&1 || true;
-# 	fi
-# done
 
 # MAIN System schemas: we have placed our override file in this directory
 # Sending any "error" to null (if key not found don't want to worry user)
@@ -122,23 +90,10 @@ glib-compile-schemas /usr/share/glib-2.0/schemas/ > /dev/null 2>&1 || true;
 # ------------------------------------------------------------------------------
 # Setting initial Nautilus config
 # ------------------------------------------------------------------------------
-echo
 echo "*** Setting initial Nautilus config"
-echo
-# filemanager-actions has no system config file, so copy user config to all
-# jammy: filemanager-actions no longer available in 22.04. Use Nautilus Scripts instead.
-#	existing users' .config folders.
-# users=$(find /home/* -maxdepth 0 -type d | cut -d '/' -f3)
-# while IFS= read -r user; do
-#     if [[ $(grep "$user:" /etc/passwd) ]]; then
-#         mkdir -p -m 755 "/home/$user/.config/filemanager-actions"
-#         cp /etc/skel/.config/filemanager-actions/filemanager-actions.conf "/home/$user/.config/filemanager-actions/filemanager-actions.conf"
-#         chown -R $user:$user "/home/$user/.config/filemanager-actions"
-#         chmod 644 "/home/$user/.config/filemanager-actions/filemanager-actions.conf"
-#     fi
-# done <<< "$users"
 
 # Update config for existing users.
+#	 Future user config copied to /etc/skel by debian/install.
 users=$(find /home/* -maxdepth 0 -type d | cut -d '/' -f3)
 src_scripts_dir="${DIR}/nautilus-scripts"
 src_templates_dir="${DIR}/templates"
@@ -160,18 +115,10 @@ while read -r user; do
     fi
 done <<< "$users"
 
-# Update config for future users.
-# sys_scripts_dir="/etc/skel/.local/share/nautilus/scripts"
-# mkdir --parents "$sys_scripts_dir"
-# cp --recursive "${src_scripts_dir}/"* "$sys_scripts_dir"
-# sys_templates_dir="/etc/skel/Templates"
-# mkdir --parents "$sys_templates_dir"
-# cp --recursive "${src_templates_dir}/"* "$sys_templates_dir"
-
-echo
 echo "*** Need to reboot for changes to take effect."
 echo "*** \"Wasta GNOME\" will be available as an alternative session at the login screen."
 echo
+
 # Require reboot to ensure gdm3 is eventually restarted.
 echo "*** System restart required ***" > /var/run/reboot-required
 
